2ee6f6c2417877d77e07ee1417d75ee5b6d94ec7
Koino Engineering <email@koino.engineering>
2020-05-30 18:48:43 +0900

    #1 fix

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-import express from "express";
+import express, { Express } from "express";
 import router from "./router";
 
-const PORT: number = isNaN(Number(process.env.PORT)) ? 3000 : Number(process.env.PORT);
+const PORT: number = isNaN(Number(process.env.PORT)) ? 4000 : Number(process.env.PORT);
 
-express()
+const useReact = (expressInstance: Express): Express => {
+    if (process.env.NODE_ENV === "production") {
+        return expressInstance
+            .use("/react", express.static(__dirname + "/front/build"))
+            .use("/static", express.static(__dirname + "/front/build/static"));
+    } else {
+        return expressInstance;
+    }
+};
+
+useReact(express()
     .use((req, res, next) => {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -12,4 +22,6 @@ express()
     .use(express.json()) // add json perser for request body
     .use(express.urlencoded({ extended: true })) // add setting for json perser
     .use(router) // add routing
+)
     .listen(PORT, () => { console.log("listening on port " + PORT + "!"); });
+
